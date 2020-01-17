1c1726e6b931d4d53d39547947501c6baf3972b1
Alexander Mikhailenko <molotok539@gmail.com>
2020-01-17 14:27:14 +1000

    Add orders setter.

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace ASPNetCoreAPI.Controllers
 {
@@ -19,6 +20,30 @@ namespace ASPNetCoreAPI.Controllers
         {
             var result = _context.Orders.Where(m => m.CustomerId == customerId);
             return Json(result);
+        }
+
+        public string Set(string json)
+        {
+            try
+            {
+                var inputJObject = JObject.Parse(json);
+                var ordersJToken = inputJObject["orders"];
+                var contractJToken = inputJObject["contract"];
+                var detailsJToken = inputJObject["details"];
+
+                JSonHelper helper = new JSonHelper();
+                Orders orders = helper.ConvertJSonToObject<Orders>(ordersJToken.ToString());
+                Contract contract = helper.ConvertJSonToObject<Contract>(contractJToken.ToString());
+                List<Details> details = helper.ConvertJSonToObject<List<Details>>(detailsJToken.ToString());
+
+                // TODO Do adding members into tables.
+
+                return "Order is accepted";
+            }
+            catch
+            {
+                return "Error";
+            }
         }
     }
 }
